a2269b3cd77372704f516b17e8090701d27d2b9e
Yuriev Yuriy <yuriy@Mac-Yuriev.local>
2021-12-26 20:44:28 +0200

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Chat from './Chat';
-
-test('renders learn react link', () => {
-  render(<Chat />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
